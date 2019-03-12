ae7560b0c87ba1a3ecc632aa3fb0fc00e604ec3b
Santiago Laboy <msanti04@harris.com>
2019-03-12 14:28:01 -0400

    Added the style to the components and created them using html.

--- ppmtoolFullStack/ppmtool-react-client/src/components/Dashboard.js
+++ ppmtoolFullStack/ppmtool-react-client/src/components/Dashboard.js
@@ -4,9 +4,21 @@ import Projectitem from "./Project/Projectitem";
 class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <h1>Welcome to the Dashboard</h1>
-        <Projectitem />
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+              <a Link="ProjectForm.html" className="btn btn-lg btn-info">
+                Create a Project
+              </a>
+              <Projectitem />
+              <br />
+              <hr />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- ppmtoolFullStack/ppmtool-react-client/src/components/Layout/Header.js
+++ ppmtoolFullStack/ppmtool-react-client/src/components/Layout/Header.js
@@ -3,9 +3,44 @@ import React, { Component } from "react";
 class Header extends Component {
   render() {
     return (
-      <div>
-        <h1>Navbar</h1>
-      </div>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
+        <div className="container">
+          <a className="navbar-brand" Link="Dashboard.html">
+            Project Management Tool
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#mobile-nav"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          <div className="collapse navbar-collapse" id="mobile-nav">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a className="nav-link" Link="/dashboard">
+                  Dashboard
+                </a>
+              </li>
+            </ul>
+
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link " Link="register.html">
+                  Sign Up
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" Link="login.html">
+                  Login
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
