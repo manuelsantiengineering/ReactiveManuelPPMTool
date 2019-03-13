6ff15ceb6479b29cba255ec00574b11d3ce028e7
Santiago Laboy <msanti04@harris.com>
2019-03-13 15:39:11 -0400

    Finally it is possible to get all projects and present them!

--- ppmtoolFullStack/ppmtool-react-client/src/components/Dashboard.js
+++ ppmtoolFullStack/ppmtool-react-client/src/components/Dashboard.js
@@ -1,19 +1,31 @@
 import React, { Component } from "react";
 import Projectitem from "./Project/Projectitem";
 import CreateProjectButton from "./Project/CreateProjectButton";
+import { connect } from "react-redux";
+import { getProjects } from "../actions/projectActions";
+import PropTypes from "prop-types";
 
 class Dashboard extends Component {
+  // Life cycle Hook
+  componentDidMount() {
+    this.props.getProjects();
+  }
+
   render() {
+    const { projects } = this.props.project;
+
     return (
       <div className="projects">
         <div className="container">
           <div className="row">
             <div className="col-md-12">
-              <h1 className="display-4 text-center">Projects</h1>
+              <h1 className="display-4 text-center">Recipes</h1>
               <br />
               <CreateProjectButton />
               <br />
-              <Projectitem />
+              {projects.map(project => (
+                <Projectitem key={project.id} project={project} />
+              ))}
               <br />
               <hr />
             </div>
@@ -24,4 +36,16 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard;
+Dashboard.propTypes = {
+  project: PropTypes.object.isRequired,
+  getProjects: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  project: state.project
+});
+
+export default connect(
+  mapStateToProps,
+  { getProjects }
+)(Dashboard);
