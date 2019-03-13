97f0944ba7792d0a91fee759786d5fcbef346462
Santiago Laboy <msanti04@harris.com>
2019-03-13 16:15:35 -0400

    Added the update project form and component.

--- ppmtoolFullStack/ppmtool-react-client/src/components/Project/Projectitem.js
+++ ppmtoolFullStack/ppmtool-react-client/src/components/Project/Projectitem.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Projectitem extends Component {
   render() {
@@ -18,15 +19,19 @@ class Projectitem extends Component {
             <div className="col-md-4 d-none d-lg-block">
               <ul className="list-group">
                 <a href="#">
-                  <li className="list-group-item board">
-                    <i className="fa fa-flag-checkered pr-1">Project Board</i>
-                  </li>
-                </a>
-                <a href="#">
                   <li className="list-group-item update">
-                    <i className="fa fa-edit pr-1">Update Project Info</i>
+                    <i className="fa fa-edit pr-1">Project Board</i>
                   </li>
                 </a>
+
+                <Link to={`/updateProject/${project.projectIdentifier}`}>
+                  <li className="list-group-item board">
+                    <i className="fa fa-flag-checkered pr-1">
+                      Update Project Info
+                    </i>
+                  </li>
+                </Link>
+
                 <a href="">
                   <li className="list-group-item delete">
                     <i className="fa fa-minus-circle pr-1">Delete Project</i>
