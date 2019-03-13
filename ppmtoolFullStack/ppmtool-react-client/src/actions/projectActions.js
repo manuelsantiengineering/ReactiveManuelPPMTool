5b920cc9e48dee985417cf41a4812e8275f1bcef
Santiago Laboy <msanti04@harris.com>
2019-03-13 13:25:52 -0400

    Added @CrossOrigin annotation to the Project Controller. Also made changes to the Project actions to store the errors.

--- ppmtoolFullStack/ppmtool-react-client/src/actions/projectActions.js
+++ ppmtoolFullStack/ppmtool-react-client/src/actions/projectActions.js
@@ -3,7 +3,7 @@ import { GET_ERRORS } from "./types";
 
 export const createProject = (project, history) => async dispatch => {
   try {
-    const res = await axios.post("htt[://localhost:8080/api/project", project);
+    const res = await axios.post("http://localhost:8080/api/project", project);
     history.push("/dashboard");
   } catch (err) {
     dispatch({
--- ppmtoolFullStack/ppmtool-react-client/src/components/Project/Projectitem.js
+++ ppmtoolFullStack/ppmtool-react-client/src/components/Project/Projectitem.js
@@ -15,17 +15,17 @@ class Projectitem extends Component {
             </div>
             <div className="col-md-4 d-none d-lg-block">
               <ul className="list-group">
-                <a Link="#">
+                <a href="#">
                   <li className="list-group-item board">
                     <i className="fa fa-flag-checkered pr-1">Project Board</i>
                   </li>
                 </a>
-                <a Link="#">
+                <a href="#">
                   <li className="list-group-item update">
                     <i className="fa fa-edit pr-1">Update Project Info</i>
                   </li>
                 </a>
-                <a Link="">
+                <a href="">
                   <li className="list-group-item delete">
                     <i className="fa fa-minus-circle pr-1">Delete Project</i>
                   </li>
