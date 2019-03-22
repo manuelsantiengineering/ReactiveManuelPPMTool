3f30a47cca4d6413f817fb0a1c549dc0bede7634
Manuel Santiago <manuelsantiengineering@gmail.com>
2019-03-22 13:50:38 -0400

    Changed security timeout constant to 60 seconds.

--- ppmtoolFullStack/ppmtool/src/main/java/com/reactivemanuel/ppmtool/security/SecurityConstants.java
+++ ppmtoolFullStack/ppmtool/src/main/java/com/reactivemanuel/ppmtool/security/SecurityConstants.java
@@ -6,8 +6,8 @@ public class SecurityConstants {
 	public static final String 	TOKEN_PREFIX 	= "Bearer ";
 	public static final String 	HEADER_STRING 	= "Authorization";
 	
-//	public static final long 	EXPIRATION_TIME	= 60_000; //About 30 sec
-	public static final long 	EXPIRATION_TIME	= 90_900_000;
+	public static final long 	EXPIRATION_TIME	= 60_000; //About 30 sec
+//	public static final long 	EXPIRATION_TIME	= 90_900_000;
 	
 	public static final String 	SIGNUP_UP_URLS 	= "/api/users/**";
 	public static final String 	H2_URL 			= "h2-console/**";
