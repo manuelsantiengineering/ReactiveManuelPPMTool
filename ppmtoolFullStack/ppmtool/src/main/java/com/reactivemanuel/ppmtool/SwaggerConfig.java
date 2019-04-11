06f1e3ff0ae9f2754b31a0a616bb6ce600b070ac
Santiago Laboy <msanti04@harris.com>
2019-04-11 10:33:29 -0400

    Using different springfox versions. It seems like with the 2.9.2 version it is adding a name query which does not appear with 2.8.0

--- ppmtoolFullStack/ppmtool/src/main/java/com/reactivemanuel/ppmtool/SwaggerConfig.java
+++ ppmtoolFullStack/ppmtool/src/main/java/com/reactivemanuel/ppmtool/SwaggerConfig.java
@@ -5,17 +5,18 @@ import static springfox.documentation.builders.PathSelectors.regex;
 import java.util.Date;
 import java.util.List;
 
-import com.fasterxml.classmate.TypeResolver;
+//import com.fasterxml.classmate.TypeResolver;
 //import org.joda.time.LocalDate;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Pageable;
 //import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.context.request.async.DeferredResult;
+//import org.springframework.web.context.request.async.DeferredResult;
 
 import com.google.common.base.Predicate;
 import com.google.common.collect.Lists;
@@ -26,19 +27,19 @@ import com.reactivemanuel.ppmtool.web.UserController;
 
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
+//import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.schema.WildcardType;
+//import springfox.documentation.schema.ModelRef;
+//import springfox.documentation.schema.WildcardType;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.service.SecurityReference;
-import springfox.documentation.service.Tag;
+//import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -64,70 +65,70 @@ import static com.google.common.base.Predicates.or;
 @Configuration
 @Import(BeanValidatorPluginsConfiguration.class)
 
-@ComponentScan(basePackageClasses = {
-	    BacklogController.class,
-	    ProjectController.class,
-	    UserController.class
-	})
-
+//@ComponentScan(basePackageClasses = {
+//	    BacklogController.class,
+//	    ProjectController.class,
+//	    UserController.class
+//	})
 public class SwaggerConfig {
 	
 	public static final String AUTHORIZATION_HEADER 	= "Authorization";
     public static final String DEFAULT_INCLUDE_PATTERN 	= "/api/.*";
 
-    @Autowired
-    private TypeResolver typeResolver;
+//    @Autowired
+//    private TypeResolver typeResolver;
         
-    @Bean
-    public Docket api() {
-      return new Docket(DocumentationType.SWAGGER_2)
-          .select()
-          .apis(RequestHandlerSelectors.basePackage("com.reactivemanuel.ppmtool.web"))
-          .paths(regex(DEFAULT_INCLUDE_PATTERN))
-          .build()
-          .pathMapping("/")
-          .directModelSubstitute(java.time.LocalDate.class, java.sql.Date.class)
-		  .directModelSubstitute(java.time.ZonedDateTime.class, Date.class)
-		  .directModelSubstitute(java.time.LocalDateTime.class, Date.class)	
-          .genericModelSubstitutes(ResponseEntity.class)
-          .useDefaultResponseMessages(false)
-		  .globalResponseMessage(RequestMethod.GET,defaultGetResponses())
-		  .globalResponseMessage(RequestMethod.POST,defaultPostResponses())
-		  .globalResponseMessage(RequestMethod.DELETE,defaultGetResponses())
-		  .globalResponseMessage(RequestMethod.PATCH,defaultGetResponses())
-          .securitySchemes(Lists.newArrayList(apiKey()))
-          .securityContexts(Lists.newArrayList(securityContext()))
-          .enableUrlTemplating(true)
-          .apiInfo(apiInfo()); 
-    }
-    
-    
-//	@Bean
-//    public Docket api() { 
-//        return new Docket(DocumentationType.SWAGGER_2)
-//        	  .select()  
-//        	  .apis(RequestHandlerSelectors.basePackage("com.reactivemanuel.ppmtool.web"))
-//        	  .paths(regex(DEFAULT_INCLUDE_PATTERN))  
-//        	  .build()        	  
-//              .pathMapping("/")
-//              .directModelSubstitute(java.time.LocalDate.class, java.sql.Date.class)
-//			  .directModelSubstitute(java.time.ZonedDateTime.class, Date.class)
-//			  .directModelSubstitute(java.time.LocalDateTime.class, Date.class)	
-//			  .genericModelSubstitutes(ResponseEntity.class)		  
-//			  .useDefaultResponseMessages(false)
-//			  .globalResponseMessage(RequestMethod.GET,defaultGetResponses())
-//			  .globalResponseMessage(RequestMethod.POST,defaultPostResponses())
-//			  .globalResponseMessage(RequestMethod.DELETE,defaultGetResponses())
-//			  .globalResponseMessage(RequestMethod.PATCH,defaultGetResponses())
-//			  .securitySchemes(Lists.newArrayList(apiKey()))
-//			  .securityContexts(Lists.newArrayList(securityContext()))
-//			  
-////			  .forCodeGeneration(true)			  			 
-////			  .ignoredParameterTypes(Pageable.class)
-////			  .ignoredParameterTypes(java.sql.Date.class)
-//
-//			  .apiInfo(apiInfo());                                           
+//    @Bean
+//    public Docket api() {
+//      return new Docket(DocumentationType.SWAGGER_2)
+//          .select()
+//          .apis(RequestHandlerSelectors.basePackage("com.reactivemanuel.ppmtool.web"))
+//          .paths(regex(DEFAULT_INCLUDE_PATTERN))
+//          .build()
+//          .pathMapping("/")
+//          .directModelSubstitute(java.time.LocalDate.class, java.sql.Date.class)
+//		  .directModelSubstitute(java.time.ZonedDateTime.class, Date.class)
+//		  .directModelSubstitute(java.time.LocalDateTime.class, Date.class)	
+//          .genericModelSubstitutes(ResponseEntity.class)
+//          .useDefaultResponseMessages(false)
+//		  .globalResponseMessage(RequestMethod.GET,defaultGetResponses())
+//		  .globalResponseMessage(RequestMethod.POST,defaultPostResponses())
+//		  .globalResponseMessage(RequestMethod.DELETE,defaultGetResponses())
+//		  .globalResponseMessage(RequestMethod.PATCH,defaultGetResponses())
+//          .securitySchemes(Lists.newArrayList(apiKey()))
+//          .securityContexts(Lists.newArrayList(securityContext()))
+//          .enableUrlTemplating(true)
+//          .apiInfo(apiInfo()); 
 //    }
+    
+    
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)
+        	  .select()  
+        	  .apis(or(RequestHandlerSelectors.basePackage("com.reactivemanuel.ppmtool.web"),
+        			  RequestHandlerSelectors.basePackage("com.reactivemanuel.ppmtool.repositories")))
+        	  .paths(regex(DEFAULT_INCLUDE_PATTERN))  
+        	  .build()        	  
+              .pathMapping("/")
+              .directModelSubstitute(java.time.LocalDate.class, java.sql.Date.class)
+			  .directModelSubstitute(java.time.ZonedDateTime.class, Date.class)
+			  .directModelSubstitute(java.time.LocalDateTime.class, Date.class)	
+			  .genericModelSubstitutes(ResponseEntity.class)		  
+			  .useDefaultResponseMessages(false)
+			  .globalResponseMessage(RequestMethod.GET,defaultGetResponses())
+			  .globalResponseMessage(RequestMethod.POST,defaultPostResponses())
+			  .globalResponseMessage(RequestMethod.DELETE,defaultGetResponses())
+			  .globalResponseMessage(RequestMethod.PATCH,defaultGetResponses())
+			  .securitySchemes(Lists.newArrayList(apiKey()))
+			  .securityContexts(Lists.newArrayList(securityContext()))
+			  
+			  .forCodeGeneration(true)			  			 
+			  .ignoredParameterTypes(Pageable.class)
+			  .ignoredParameterTypes(java.sql.Date.class)
+
+			  .apiInfo(apiInfo());                                           
+    }
     
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
@@ -148,7 +149,10 @@ public class SwaggerConfig {
 	private SecurityContext securityContext() {
         return SecurityContext.builder()
             .securityReferences(defaultAuth())
-            .forPaths(securityPaths())
+            .forPaths(or(
+		        regex("/api/project/.*"),
+		        regex("/api/backlog/.*")
+		        ))
 //            .forPaths(PathSelectors.regex(DEFAULT_INCLUDE_PATTERN))                 
             .build();
     }
@@ -176,18 +180,11 @@ public class SwaggerConfig {
 	        .operationsSorter(OperationsSorter.ALPHA)
 	        .showExtensions(false)
 	        .tagsSorter(TagsSorter.ALPHA)
-	        .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+//	        .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
 	        .validatorUrl(null)
 	        .build();
 	  }
-	
-	private Predicate<String> securityPaths() {
-	    return or(
-	        regex("/api/project/.*"),
-	        regex("/api/backlog/.*")
-	        );
-	  }
-	
+		
 	List<ResponseMessage> defaultGetResponses(){
 		return Lists.newArrayList(
 				new ResponseMessageBuilder().code(200).message("Success.").build(),
